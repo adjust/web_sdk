7df1b50fa9d18063dd0c811b92857ff38f4af5b9
Milica Kadic <milica.kadic.87@gmail.com>
2018-08-01 09:32:06 +0200

    unminify the source and some minor changes

--- adjust.js
+++ adjust.js
@@ -1,11 +1,90 @@
-(function(window){var sendRequest=function(method,url,data,success_cb,error_cb){var req=new XMLHttpRequest();req.open(method,url,!0);req.setRequestHeader("Client-SDK","js4.0.0");req.onreadystatechange=function(){if(req.readyState==4){if(req.status>=200&&req.status<400){!!success_cb&&success_cb(req.responseText)}else if(!!error_cb){!!error_cb&&error_cb(new Error("Server responded with HTTP "+req.status),xhr)}}};if(!!error_cb){req.onerror=error_cb}
-req.send(data)}
-var encodeQueryString=function(params){var pairs=[];for(var k in params){if(!params.hasOwnProperty(k)){continue}
-pairs.push(encodeURIComponent(k)+"="+encodeURIComponent(params[k]))}
-return pairs.join("&")}
-var cloneObj=function(obj){var copy={};if(typeof(obj)!="object"||!obj){return copy}
-for(var k in obj){if(!obj.hasOwnProperty(k)){continue}
-copy[k]=obj[k]}
-return copy}
-if(!'withCredentials' in new XMLHttpRequest()){sendRequest=function(){}}
-window.Adjust=function(app_token,environment,os_name){this.trackSession=function(device_ids){var params=cloneObj(device_ids);params.app_token=app_token;params.os_name=os_name;params.environment=environment;sendRequest("GET","https://app.adjust.com/session?"+encodeQueryString(params))};this.trackEvent=function(event_token,device_ids){var params=cloneObj(device_ids);params.app_token=app_token;params.event_token=event_token;params.os_name=os_name;params.environment=environment;sendRequest("GET","https://app.adjust.com/event?"+encodeQueryString(params))}}})(window)
+(function (window) {
+
+  function sendRequest(method, url, data, success_cb, error_cb) {
+
+    var req = new XMLHttpRequest();
+
+    req.open(method, url, !0);
+    req.setRequestHeader('Client-SDK', 'js4.0.0');
+    req.onreadystatechange = function () {
+      if (req.readyState === 4) {
+        if (req.status >= 200 && req.status < 400) {
+          !!success_cb && success_cb(req.responseText);
+        } else if (!!error_cb) {
+          !!error_cb && error_cb(new Error('Server responded with HTTP ' + req.status), xhr);
+        }
+      }
+    };
+
+    if (!!error_cb) {
+      req.onerror = error_cb
+    }
+
+    req.send(data)
+  }
+
+  function encodeQueryString(params) {
+    var pairs = [];
+    for (var k in params) {
+      if (!params.hasOwnProperty(k)) {
+        continue
+      }
+      pairs.push(encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+    }
+    return pairs.join('&')
+  }
+
+  function cloneObj(obj) {
+    var copy = {};
+    if (typeof(obj) !== 'object' || !obj) {
+      return copy
+    }
+    for (var k in obj) {
+      if (!obj.hasOwnProperty(k)) {
+        continue
+      }
+      copy[k] = obj[k]
+    }
+    return copy
+  }
+
+  if (!'withCredentials' in new XMLHttpRequest()) {
+    sendRequest = function () {}
+  }
+
+  window.Adjust = function Adjust(options) {
+
+    var _appToken = options.app_token;
+    var _environment = options.environment;
+    var _osName = options.os_name || 'android';
+
+    return {
+      trackSession: trackSession,
+      trackEvent: trackEvent
+    };
+
+    function trackSession(options, onSuccess, onError) {
+
+      var params = cloneObj(options.device_ids);
+      params.app_token = _appToken;
+      params.environment = _environment;
+      params.os_name = _osName;
+
+      sendRequest('GET', 'https://app.adjust.com/session?' + encodeQueryString(params), null, onSuccess, onError);
+
+    }
+
+    function trackEvent(options, onSuccess, onError) {
+
+      var params = cloneObj(options.device_ids);
+      params.event_token = options.event_token;
+      params.app_token = _appToken;
+      params.environment = _environment;
+      params.os_name = _osName;
+
+      sendRequest('GET', 'https://app.adjust.com/event?' + encodeQueryString(params), null, onSuccess, onError)
+    }
+
+  }
+
+})(window);
--- example.js
+++ example.js
@@ -1,12 +1,27 @@
 // Example usage
-var app_token = "rb4g27fje5ej",
-   environment = "production", // or "sandbox"
-   event_token = "uqg17r",
-   os_name = 'android'
-   device_ids = {
-     "gps_adid": "5056e23a-dc1d-418f-b5a2-4ab3e75daab2"
-   };
+var device_ids = {
+  gps_adid: '5056e23a-dc1d-418f-b5a2-4ab3e75daab2'
+};
 
-var adjust = new Adjust(app_token, environment, "android");
-adjust.trackSession(device_ids);
-adjust.trackEvent(event_token, device_ids);
+var adjust = new Adjust({
+  app_token: 'rb4g27fje5ej',
+  environment: 'production', // or 'sandbox'
+  os_name: 'android'
+});
+
+adjust.trackSession({
+  device_ids: device_ids
+}, function () {
+  console.log('success');
+}, function () {
+  console.log('error');
+});
+
+adjust.trackEvent({
+  event_token: 'uqg17r',
+  device_ids: device_ids
+}, function () {
+  console.log('success');
+}, function () {
+  console.log('error');
+});
