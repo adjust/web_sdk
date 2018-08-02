0815b367f47bc484265a77d3e8e9991523592705
Milica Kadic <milica.kadic.87@gmail.com>
2018-08-02 09:44:43 +0200

    demo + support for revenue, callback and partner params

--- dist/adjust.js
+++ dist/adjust.js
@@ -1,17 +1,18 @@
 (function (window) {
+  'use strict';
 
   function sendRequest(method, url, data, success_cb, error_cb) {
 
     var req = new XMLHttpRequest();
 
     req.open(method, url, !0);
-    req.setRequestHeader('Client-SDK', 'js4.0.0');
+    req.setRequestHeader('Client-SDK', 'js4.1.0');
     req.onreadystatechange = function () {
       if (req.readyState === 4) {
         if (req.status >= 200 && req.status < 400) {
-          !!success_cb && success_cb(req.responseText);
+          !!success_cb && success_cb(req);
         } else if (!!error_cb) {
-          !!error_cb && error_cb(new Error('Server responded with HTTP ' + req.status), xhr);
+          !!error_cb && error_cb(new Error('Server responded with HTTP ' + req.status), req);
         }
       }
     };
@@ -54,6 +55,8 @@
 
   window.Adjust = function Adjust(options) {
 
+    options = options || {};
+
     var _appToken = options.app_token;
     var _environment = options.environment;
     var _osName = options.os_name || 'android';
@@ -65,7 +68,10 @@
 
     function trackSession(options, onSuccess, onError) {
 
+      options = options || {};
+
       var params = cloneObj(options.device_ids);
+
       params.app_token = _appToken;
       params.environment = _environment;
       params.os_name = _osName;
@@ -76,13 +82,66 @@
 
     function trackEvent(options, onSuccess, onError) {
 
+      options = options || {};
+
+      var revenue = _getRevenue(options);
+      var callbackParams = _getMap(options.callback_params);
+      var partnerParams = _getMap(options.partner_params);
       var params = cloneObj(options.device_ids);
+
+      if (revenue) {
+        params.revenue = revenue.revenue;
+        params.currency = revenue.currency;
+      }
+
+      if (callbackParams) {
+        params.callback_params = JSON.stringify(callbackParams);
+      }
+
+      if (partnerParams) {
+        params.partner_params = JSON.stringify(partnerParams);
+      }
+
       params.event_token = options.event_token;
       params.app_token = _appToken;
       params.environment = _environment;
       params.os_name = _osName;
 
       sendRequest('GET', 'https://app.adjust.com/event?' + encodeQueryString(params), null, onSuccess, onError)
+
+    }
+
+    function _getRevenue(options) {
+
+      var revenue = parseFloat(options.revenue);
+
+      if (revenue < 0 || !options.currency) {
+        return null;
+      }
+
+      return {
+        revenue: revenue.toFixed(5),
+        currency: options.currency
+      };
+
+    }
+
+    function _getMap(params) {
+
+      params = params || [];
+
+      if (!params.length) {
+        return null;
+      }
+
+      var map = {};
+
+      for (var i = 0; i < params.length; i++) {
+        map[params[i].key] = params[i].value;
+      }
+
+      return cloneObj(map);
+
     }
 
   }
