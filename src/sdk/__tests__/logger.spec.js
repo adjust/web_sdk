64d27206468407aca9c566faee18e9412cd32dff
Milica Kadic <milica.kadic.87@gmail.com>
2019-04-23 13:52:38 +0200

    simple logging system

---
+++ src/sdk/__tests__/logger.spec.js
@@ -0,0 +1,194 @@
+/* eslint-disable */
+import * as Logger from '../logger'
+
+describe('test Logger functionality', () => {
+
+  beforeAll(() => {
+    jest.spyOn(console, 'log').mockImplementation(() => {})
+    jest.spyOn(console, 'info').mockImplementation(() => {})
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  afterAll(() => {
+    global.__ADJUST__ENV = 'test'
+  })
+
+  describe('verbose log level by default in test environment', () => {
+
+    it('prints verbose messages', () => {
+      Logger.default.log('Some message with looooong explanation')
+      expect(console.log).toHaveBeenCalledWith('LOG:', 'Some message with looooong explanation')
+
+    })
+
+    it('prints info messages', () => {
+      Logger.default.info('Some message')
+      expect(console.info).toHaveBeenCalledWith('INFO:', 'Some message')
+    })
+
+    it('prints error messages', () => {
+      Logger.default.error('Some error message')
+      expect(console.error).toHaveBeenCalledWith('ERROR:', 'Some error message')
+    })
+
+  })
+
+  describe('verbose log level by default in development environment', () => {
+
+    beforeAll(() => {
+      global.__ADJUST__ENV = 'development'
+      Logger.default.setLogLevel()
+    })
+
+    afterAll(() => {
+      global.__ADJUST__ENV = 'test'
+    })
+
+    it('prints verbose messages', () => {
+      Logger.default.log('Some message with looooong explanation')
+      expect(console.log).toHaveBeenCalledWith('LOG:', 'Some message with looooong explanation')
+
+    })
+
+    it('prints info messages', () => {
+      Logger.default.info('Some message')
+      expect(console.info).toHaveBeenCalledWith('INFO:', 'Some message')
+    })
+
+    it('prints error messages', () => {
+      Logger.default.error('Some error message')
+      expect(console.error).toHaveBeenCalledWith('ERROR:', 'Some error message')
+    })
+
+  })
+
+  describe('error log level by default in production environment', () => {
+
+    beforeAll(() => {
+      global.__ADJUST__ENV = 'production'
+      Logger.default.setLogLevel()
+    })
+
+    afterAll(() => {
+      global.__ADJUST__ENV = 'test'
+    })
+
+    it('does not print verbose messages', () => {
+      Logger.default.log('Some message with looooong explanation')
+      expect(console.log).not.toHaveBeenCalled()
+
+    })
+
+    it('does not print info messages', () => {
+      Logger.default.info('Some message')
+      expect(console.info).not.toHaveBeenCalled()
+    })
+
+    it('prints error messages', () => {
+      Logger.default.error('Some error message')
+      expect(console.error).toHaveBeenCalledWith('ERROR:', 'Some error message')
+    })
+
+  })
+
+  describe('explicit verbose log level', () => {
+
+    beforeAll(() => {
+      Logger.default.setLogLevel('verbose')
+    })
+
+    it('prints verbose messages', () => {
+      Logger.default.log('Some message with looooong explanation')
+      expect(console.log).toHaveBeenCalledWith('LOG:', 'Some message with looooong explanation')
+
+    })
+
+    it('prints info messages', () => {
+      Logger.default.info('Some message')
+      expect(console.info).toHaveBeenCalledWith('INFO:', 'Some message')
+    })
+
+    it('prints error messages', () => {
+      Logger.default.error('Some error message')
+      expect(console.error).toHaveBeenCalledWith('ERROR:', 'Some error message')
+    })
+
+  })
+
+  describe('explicit info log level', () => {
+
+    beforeAll(() => {
+      Logger.default.setLogLevel('info')
+    })
+
+    it('does not print verbose messages', () => {
+      Logger.default.log('Some message with looooong explanation')
+      expect(console.log).not.toHaveBeenCalled()
+
+    })
+
+    it('prints info messages', () => {
+      Logger.default.info('Some message')
+      expect(console.info).toHaveBeenCalledWith('INFO:', 'Some message')
+    })
+
+    it('prints error messages', () => {
+      Logger.default.error('Some error message')
+      expect(console.error).toHaveBeenCalledWith('ERROR:', 'Some error message')
+    })
+
+  })
+
+  describe('explicit error log level', () => {
+
+    beforeAll(() => {
+      Logger.default.setLogLevel('error')
+    })
+
+    it('does not print verbose messages', () => {
+      Logger.default.log('Some message with looooong explanation')
+      expect(console.log).not.toHaveBeenCalled()
+
+    })
+
+    it('does not print info messages', () => {
+      Logger.default.info('Some message')
+      expect(console.log).not.toHaveBeenCalled()
+    })
+
+    it('prints error messages', () => {
+      Logger.default.error('Some error message')
+      expect(console.error).toHaveBeenCalledWith('ERROR:', 'Some error message')
+    })
+
+  })
+
+  describe('explicit none log level', () => {
+
+    beforeAll(() => {
+      Logger.default.setLogLevel('none')
+    })
+
+    it('does not print verbose messages', () => {
+      Logger.default.log('Some message with looooong explanation')
+      expect(console.log).not.toHaveBeenCalled()
+
+    })
+
+    it('does not print info messages', () => {
+      Logger.default.info('Some message')
+      expect(console.log).not.toHaveBeenCalled()
+    })
+
+    it('prints error messages', () => {
+      Logger.default.error('Some error message')
+      expect(console.error).not.toHaveBeenCalled()
+    })
+
+  })
+
+})
---
+++ src/sdk/logger.js
@@ -0,0 +1,87 @@
+const LEVEL_VERBOSE = 'verbose'
+const LEVEL_INFO = 'info'
+const LEVEL_ERROR = 'error'
+const LEVEL_NONE = 'none'
+
+/**
+ * Logger levels
+ * - none -> nothing is printed to the console
+ * - error -> prints only error
+ * - info -> prints info and error
+ * - verbose -> prints log, info and error
+ *
+ * @type {Object}
+ * @private
+ */
+const _levels = {
+  [LEVEL_NONE]: -1,
+  [LEVEL_ERROR]: 0,
+  [LEVEL_INFO]: 1,
+  [LEVEL_VERBOSE]: 2
+}
+
+/**
+ * Default logger level per environment
+ *
+ * @type {Object}
+ * @private
+ */
+const _envLogLevels = {
+  development: LEVEL_VERBOSE,
+  production: LEVEL_ERROR,
+  test: LEVEL_VERBOSE
+}
+
+/**
+ * Available logger methods
+ *
+ * @type {Array}
+ * @private
+ */
+const _methods = [
+  {name: 'log', level: LEVEL_VERBOSE},
+  {name: 'info', level: LEVEL_INFO},
+  {name: 'error', level: LEVEL_ERROR}
+]
+
+/**
+ * Current logger level
+ */
+let _current = _getDefaultLogLevel()
+
+/**
+ * Get default logger error per environment and fallback to error level when unknown env
+ *
+ * @returns {string}
+ * @private
+ */
+function _getDefaultLogLevel () {
+  return _envLogLevels[__ADJUST__ENV] || LEVEL_ERROR
+}
+
+/**
+ * Set logger level, fallback to default log level
+ *
+ * @param {string=} logLevel
+ */
+function setLogLevel (logLevel) {
+  _current = Object.keys(_levels).indexOf(logLevel) !== -1
+    ? logLevel
+    : _getDefaultLogLevel()
+}
+
+const Logger = {
+  setLogLevel
+}
+
+_methods.forEach(method => {
+  Logger[method.name] = message => {
+    if (_levels[_current] >= _levels[method.level]) {
+      console[method.name](`${method.name.toUpperCase()}:`, message) // eslint-disable-line
+    }
+  }
+})
+
+Object.freeze(Logger)
+
+export default Logger
