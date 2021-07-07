0712feedc82654ae52c0904077c0fd68986a0564
Yara Matkova <yaroslavna.matkova@akvelon.com>
2021-07-08 00:36:25 +0300

    more specific selectors for demo app styles

--- src/sdk/smart-banner/assets/template.ts
+++ src/sdk/smart-banner/assets/template.ts
@@ -1,9 +1,9 @@
 import styles from './styles.scss'
 
-// TODO: replace .adjust-logo with a correct one to show client's image
 export default (header: string, description: string, buttonText: string) => `
   <div class=${styles.bannerBody}>
     <button class="dismiss"></button>
+    <div class="${styles.appIcon}"></div>
     <div class="${styles.textContainer}">
       <h4 class="${styles.bannerText}">${header}</h4>
       <p class="${styles.bannerText}">${description}</p>
