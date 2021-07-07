4a8f7b3abe485fb740d6513fec8c177ffbc4998f
Yara Matkova <yaroslavna.matkova@akvelon.com>
2021-07-08 00:31:41 +0300

    dismiss button added

--- src/sdk/smart-banner/assets/template.ts
+++ src/sdk/smart-banner/assets/template.ts
@@ -3,10 +3,10 @@ import styles from './styles.scss'
 // TODO: replace .adjust-logo with a correct one to show client's image
 export default (header: string, description: string, buttonText: string) => `
   <div class=${styles.bannerBody}>
-    <i class="adjust-logo"></i>
+    <button class="dismiss"></button>
     <div class="${styles.textContainer}">
       <h4 class="${styles.bannerText}">${header}</h4>
       <p class="${styles.bannerText}">${description}</p>
     </div>
-    <button>${buttonText}</button>
+    <button class="action">${buttonText}</button>
   </div>`
