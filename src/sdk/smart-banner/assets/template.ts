fa676df753d5848a75519b0f1ed8dd35dd88f4f3
Yara Matkova <yaroslavna.matkova@akvelon.com>
2021-07-08 11:16:25 +0300

    fixed class names

--- src/sdk/smart-banner/assets/template.ts
+++ src/sdk/smart-banner/assets/template.ts
@@ -2,11 +2,11 @@ import styles from './styles.scss'
 
 export default (header: string, description: string, buttonText: string) => `
   <div class=${styles.bannerBody}>
-    <button class="dismiss"></button>
+    <button class="${styles.dismiss}"></button>
     <div class="${styles.appIcon}"></div>
     <div class="${styles.textContainer}">
       <h4 class="${styles.bannerText}">${header}</h4>
       <p class="${styles.bannerText}">${description}</p>
     </div>
-    <button class="action">${buttonText}</button>
+    <button class="${styles.action}">${buttonText}</button>
   </div>`
