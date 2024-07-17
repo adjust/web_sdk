6291c82fc8cb522255f443fac75144dad8aa9ffa
Yara Matkova <yara.matkova@adjust.com>
2024-07-17 12:42:41 +0200

    feat: re-implement url strategy

--- src/sdk/constants.ts
+++ src/sdk/constants.ts
@@ -24,39 +24,6 @@ export enum STORAGE_TYPES {
   LOCAL_STORAGE = 'localStorage'
 }
 
-export const ENDPOINTS = {
-  default: {
-    endpointName: 'Default',
-    app: 'https://app.adjust.com',
-    gdpr: 'https://gdpr.adjust.com'
-  },
-  india: {
-    endpointName: 'Indian',
-    app: 'https://app.adjust.net.in',
-    gdpr: 'https://gdpr.adjust.net.in'
-  },
-  china: {
-    endpointName: 'Chinese',
-    app: 'https://app.adjust.world',
-    gdpr: 'https://gdpr.adjust.world'
-  },
-  EU: {
-    endpointName: 'EU',
-    app: 'https://app.eu.adjust.com',
-    gdpr: 'https://gdpr.eu.adjust.com'
-  },
-  TR: {
-    endpointName: 'TR',
-    app: 'https://app.tr.adjust.com',
-    gdpr: 'https://gdpr.tr.adjust.com'
-  },
-  US: {
-    endpointName: 'US',
-    app: 'https://app.us.adjust.com',
-    gdpr: 'https://gdpr.us.adjust.com'
-  }
-}
-
 export const PUB_SUB_EVENTS = {
   WEB_UUID_CREATED: 'activity:web_uuid',
   ATTRIBUTION_RECEIVED: 'activity:attribution',
